3090bbfaee4dff9b96351dfc24614990eb03c19b
JuanPalbo <134970060+JuanPalbo@users.noreply.github.com>
2024-11-29 01:24:50 -0300

    boton logout

---
+++ Frontend/src/components/Logout/Logout.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Button } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+import './Logout.css';
+
+const Logout = () => {
+    const navigate = useNavigate();
+
+    const handleLogout = () => {
+        localStorage.removeItem('access_token');
+        localStorage.removeItem('user_role');
+        navigate('/login');
+    };
+
+    return (
+        <Button
+            variant="contained"
+            className="logout-button"
+            onClick={handleLogout}
+        >
+            Cerrar Sesión
+        </Button>
+    );
+}
+
+export default Logout;
+    
